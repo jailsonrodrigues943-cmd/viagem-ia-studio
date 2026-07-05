cc0fa91aa3f9298de52b67ceab1387bd3c0e02aa
jailsonrodrigues943-cmd <jailsonrodrigues943@gmail.com>
2026-07-05 14:20:50 -0300

    Primeira estrutura do app

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Viagem AI Studio iniciado!");
+
+document.querySelector("button").addEventListener("click", function () {
+    alert("Em breve o gerador de roteiros estará funcionando!");
+    });
